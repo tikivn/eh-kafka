8d8ed701faef9d96c6aa997dadf5d8e8d593a94c
Nguyễn Xuân Thứ <thu.nguyen9@tiki.vn>
2020-05-04 10:10:57 +0700

    Add feature config for batch processing

--- eventbus_test.go
+++ eventbus_test.go
@@ -61,3 +61,45 @@ func TestEventBus(t *testing.T) {
 
 	eventbus.AcceptanceTest(t, bus1, bus2, timeout)
 }
+
+func TestBatchProducerEventBus(t *testing.T) {
+	defer goleak.VerifyNoLeaks(t)
+	logger := kitlog.NewLogfmtLogger(os.Stdout)
+	logger = kitlog.With(logger, "TestEventBus", os.Getegid())
+
+	kfbus.Logger = logger
+	brokers := strings.Split("uat-kafka-1.svr.tiki.services:9092", ",")
+	topic := uuid.New()
+
+	timeout := time.Second * 60
+
+	ctx := context.Background()
+
+	bus1, err := kfbus.NewEventBus(
+		ctx,
+		brokers,
+		kfbus.DefaultTopicProducer(topic.String()),
+		kfbus.DefaultTopicsConsumer(topic.String()),
+		kfbus.WithTimeout(timeout),
+	)
+	if err != nil {
+		t.Fatal("there should be no error:", err)
+	}
+	bus1.SetWaitConsumer(true)
+	defer bus1.Close()
+
+	bus2, err := kfbus.NewEventBusWithBatchProducer(
+		ctx,
+		brokers,
+		kfbus.DefaultTopicProducer(topic.String()),
+		kfbus.DefaultTopicsConsumer(topic.String()),
+		kfbus.WithTimeout(timeout),
+	)
+	if err != nil {
+		t.Fatal("there should be no error:", err)
+	}
+	bus2.SetWaitConsumer(true)
+	defer bus2.Close()
+
+	eventbus.AcceptanceTest(t, bus1, bus2, timeout)
+}
